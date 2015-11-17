3cf5e5e2a40da8eba8156faf5fb86f533c5c8eb3
smile <1181169242@qq.com>
2015-11-17 17:24:38 +0800

    11

---
+++ MyApp/app/src/main/java/com/demo/myapp/Demo1Activity.java
@@ -0,0 +1,68 @@
+package com.demo.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+public class Demo1Activity extends AppCompatActivity implements View.OnClickListener {
+    ImageView ivBoss;
+    RelativeLayout rlContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+
+        rlContainer = (RelativeLayout) findViewById(R.id.rl_container);
+        Button btnOver = (Button) findViewById(R.id.btn_click);
+        Button btnBack = (Button) findViewById(R.id.btn_back);
+        btnOver.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+
+        ivBoss = (ImageView) findViewById(R.id.iv_window);
+        ViewPropertyAnimator.animate(ivBoss).setDuration(1);
+        ViewPropertyAnimator.animate(ivBoss).alpha(0);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        ViewPropertyAnimator.animate(ivBoss).setDuration(1);
+        ViewPropertyAnimator.animate(ivBoss).alpha(1);
+        ViewPropertyAnimator.animate(ivBoss).setDuration(2000);
+        //旋转720度
+        ViewPropertyAnimator.animate(ivBoss).rotationYBy(720);
+        switch (v.getId()){
+            case R.id.btn_click:
+                ivBoss.setVisibility(View.VISIBLE);
+
+                int xValue = rlContainer.getWidth() - ivBoss.getWidth();
+                int yValue = rlContainer.getHeight() - ivBoss.getHeight();
+                ViewPropertyAnimator.animate(ivBoss).x(xValue).y(yValue);
+                ivBoss.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ivBoss.setVisibility(View.GONE);
+                    }
+                },2000);
+                break;
+            case R.id.btn_back:
+                ivBoss.setVisibility(View.VISIBLE);
+                int xValue1 = rlContainer.getWidth()/2;
+                int yValue1 = rlContainer.getHeight()/2;
+                ViewPropertyAnimator.animate(ivBoss).x(xValue1).y(yValue1);
+                ivBoss.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ivBoss.setVisibility(View.GONE);
+                    }
+                },2000);
+                break;
+
+        }
+    }
+}
--- MyApp/app/src/main/java/com/demo/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/demo/myapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.demo.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_demo1).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_demo1:
+                Intent intent = new Intent(this,Demo1Activity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+
+        }
+
     }
 }
